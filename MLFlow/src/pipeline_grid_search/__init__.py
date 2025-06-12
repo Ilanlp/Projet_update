142f5e0dc0f67d3f4f902effc0b5d4b80835b273
mika <mickaelkremer@gmail.com>
2025-06-12 19:05:50 +0200

    clean

--- MLFlow/src/pipeline_grid_search/__init__.py
+++
@@ -1,3 +0,0 @@
-"""
-Package pipeline_grid_search pour le matching d'offres et de candidats
-""" 
--- MLFlow/src/pipeline_grid_search/utils/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Package utils pour le pipeline de matching d'emplois.
-"""
-
-from .mlflow_utils import MLflowGridSearchCV
-from .scoring import matching_scorer, compute_similarity_score
-
-__all__ = ['MLflowGridSearchCV', 'matching_scorer', 'compute_similarity_score']
